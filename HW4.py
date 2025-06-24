2e3467d4ee636d9ff0784d6ddbd1ea5b709adf75
Boragozz <boragozz@gmail.com>
2025-06-24 12:02:47 +0100

    Update HW4.py
    Palindrome review

---
+++ HW4.py
@@ -0,0 +1,6 @@
+# Palindrom review
+def polindrom(text: str) -> bool:
+  revText = ''.join(char.lower() for char in text if char.isalnum()
+  return revText = revText[::-1]
+review = input("Введите слово для проверки: ")
+print(polindrom(review))
